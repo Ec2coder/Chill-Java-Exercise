4e543dd4f9cbf7af97eb5704ee6610305ae1d870
eliaspaniagua <thepaniagua01@gmail.com>
2021-03-12 00:02:05 -0400

    resolucion del ejercicio 33, exercise33.java

---
+++ DDDRbasic/exercise33.java
@@ -0,0 +1,10 @@
+//33) Dada una cadena, extraer la cuarta y quinta letra usando el método substring.
+
+package DDDRbasic;
+
+public class exercise33{
+  public static void main(String[] args) {
+  	String srt = "Programmer";
+	System.out.println(srt.substring(4,5)+srt.substring(5,6));
+  }
+}
